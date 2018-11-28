916a844260ae4b7e7a4d785fefb10d9fa7e10500
Unicourt1 <courtneytyler11@yahoo.com>
2018-11-28 00:26:59 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,33 @@
-console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "king", "queen", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+// this will hold the cards that are in play
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardsInPlay);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[cardsInPlay.length-1]);
+
+if (cardsInPlay.length === 2) {
+	// code to be exec if true
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		// code to be exec if true
+		alert("You found a match!");
+	} else {
+		// code otherwise
+		alert("Sorry, try again!");
+	}}
+ else 
+	// code otherwise
+	alert("Flip another card");
+
+
+
