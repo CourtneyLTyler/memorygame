5a52423473f0879149080ae712146e49ae8cfc60
Unicourt1 <courtneytyler11@yahoo.com>
2018-11-30 13:22:13 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,5 @@
 
+// create object with each card's properties
 var cards = [
 {
 	rank: "queen",
@@ -22,38 +23,54 @@ var cards = [
 }
 ];
 
-// this will hold the cards that are in play
+// create empty array to hold the cards while they are in play
 var cardsInPlay = [];
 
+// create fx to check if the cardsInPlay elements match and notify user
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 };
 
-var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
-	console.log(cards[cardId].cardImage);
-
+// create fx to be executed once a card is clicked
+var flipCard = function() {
+	// give the current card an ID and assign to a var
+	var cardId = this.getAttribute("data-id");
+	// add the rank of the current card to the cardsInPlay array
 	cardsInPlay.push(cards[cardId].rank);
-	if (cardsInPlay.length === 2) {
-	// code to be exec if true
+	// add the cardImage for the current card
+	this.setAttribute("src", cards[cardId].cardImage);
+	// run checkForMatch fx when there's an even number of elements in cardsInPlay array
+	if (cardsInPlay.length % 2 === 0) {
 	checkForMatch();
  } else {
-	// code otherwise
+	// if the above isn't true, send following alert to user
 	alert("Flip another card");
 }
 };
 
-flipCard(0);
-flipCard(1);
+// create fx to make the game board and add event handler
+var createBoard = function() {
+	// create for loop to iterate over the cards array
+	for (var i=0; i < cards.length; i++) {
+		// create an img element and store in the cardElement var
+		var cardElement = document.createElement("img");
+		// add content to the above img element
+		cardElement.setAttribute("src", "images/back.png");
+		// add a data-id attribute to track current card
+		cardElement.setAttribute("data-id", i);
+		// add event handler to execute flipCard fx when user clicks
+		cardElement.addEventListener("click", flipCard);
+		// add the var carElement to the element w/specified id
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+};
 
-
-
-
-
+// execute the createBoard fx
+createBoard();
 
 
 
